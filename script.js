d052d55107349be3b82c2dbdd8a4bfe343eb72ae
Aksshai Nair <67258248+aksshainair@users.noreply.github.com>
2023-03-22 18:13:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,58 +22,66 @@ s.ontouchmove = e => handleOnMove(e.touches[0]);
 
 // gsap
 
-
-
-gsap.registerPlugin(ScrollTrigger);
-let t1 = gsap.timeline({
-    default:{
-        ease:"power4.inOut",
-        duration:2
-    },
-    scrollTrigger:{
-        trigger: '.home_description',
-        toggleAction: 'restart pause none none',
-        scrub:true,
-        start:'center bottom',
-        end: '+=120 +=220',
-        // markers:true
-    },
-})
-t1.from('.home_title',{ y:60, scale:1.2, duration:1.3 })
-.from('.home_sub_div',{y:60, duration:1},'-=1.2')
-.from('.home_img',{'scale':1.4,duration:1},'-=')
-.from('.home_description',{opacity:0, 'scale':0.8 ,duration:2},'');
-
-let t2 = gsap.timeline({
-    default:{
-        ease:"power4.inOut",
-        duration:2
-    },
-    scrollTrigger:{
-        trigger: '.sec1_title',
-        // scrub:true, 
-        start:'top center',
-        end: '+=350 +=500',
-        // markers:true
-    },
-})
-t2.from('.sec1_title',{ y:70, duration:1 })
-.from('.sec1_img',{ x:250, scale:0.8, duration:2 },'-=2')
-// .from('.floating_shoe',{y:-100, scale:0.5, duration:1},'-=2')
-.from('.sec1_description',{
-    // scrollTrigger:{
-    //     trigger: '.sec1_description',
-    //     toggleAction: 'restart pause none none',
-    //     // scrub:true,
-    //     start:'top center',
-    //     end:'+=350 +=500',
-    //     markers:true
-    // }, 
-    background:'black',
-    duration:1,
-    scale:1.4,
-    color:'black',
-},'-=1');
-
+window.onload=function(){
+    w = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
+    var targetWidth = 500;
+    if ( w >= targetWidth) {     
+        gsap.registerPlugin(ScrollTrigger);
+        let t1 = gsap.timeline({
+            default:{
+                ease:"power4.inOut",
+                duration:2
+            },
+            scrollTrigger:{
+                trigger: '.centering',
+                toggleAction: 'restart pause none none',
+                scrub:true,
+                start:'center bottom',
+                end: '+=120 +=220',
+                // markers:true
+            },
+        })
+        t1.from('.home_title',{ y:60, scale:1.2, duration:1.3 })
+        .from('.home_sub_div',{y:60, duration:1},'-=1.2')
+        .from('.home_img',{'scale':1.4,duration:1},'-=')
+        .from('.centering',{opacity:0, 'scale':0.8 ,duration:2},'');
+        
+        let t2 = gsap.timeline({
+            default:{
+                ease:"power4.inOut",
+                duration:2
+            },
+            scrollTrigger:{
+                trigger: '.sec1_title',
+                // scrub:true, 
+                start:'top center',
+                end: '+=350 +=500',
+                // markers:true
+            },
+        })
+        t2.from('.sec1_title',{ y:70, duration:1 })
+        .from('.sec1_img',{ x:250, scale:0.8, duration:2 },'-=2')
+        // .from('.floating_shoe',{y:-100, scale:0.5, duration:1},'-=2')
+        .from('.sec1_description',{
+            // scrollTrigger:{
+            //     trigger: '.sec1_description',
+            //     toggleAction: 'restart pause none none',
+            //     // scrub:true,
+            //     start:'top center',
+            //     end:'+=350 +=500',
+            //     markers:true
+            // }, 
+            background:'black',
+            duration:1,
+            scale:1.4,
+            color:'black',
+        },'-=1');
+        
+        
+        
+        
+    }
+   
+    };
 
 
